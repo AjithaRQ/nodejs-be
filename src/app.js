52be6151527bb2b57b50ede96c73eddc62c384c0
AjithaRQ <164318837+AjithaRQ@users.noreply.github.com>
2025-12-16 16:51:56 +0530

    Update response message for root URL

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const startServer = async () => {
   await connectDB();
   // Define request response in root URL (/)
   app.get("/", function (req, res) {
-    res.send("Dockerize the node app");
+    res.send("Dockerize the node app - Test Comment");
   });
 
   // Define request response in root URL (/)
